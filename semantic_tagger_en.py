112d1f31ebd8aee568598c426cfb51768b54e69c
Sony Jufri <sony.jufri@sydney.edu.au>
2022-10-19 15:47:47 +1100

    Update semantic_tagger_en.py

--- semantic_tagger_en.py
+++ semantic_tagger_en.py
@@ -1445,7 +1445,7 @@ class SemanticTagger():
         n_option = widgets.BoundedIntText(
             value=value,
             min=0,
-            #max=len(self.text_df),
+            max=len(self.text_df)+1,
             step=5,
             description='',
             disabled=False,
